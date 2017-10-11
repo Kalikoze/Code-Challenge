b07fce8ce27b9731c1af7722335900a2730392b0
Travis Rollins <TROLL59910@aol.com>
2017-10-11 14:57:25 -0600

    Write functionality to check if numbers are included.  If there is a number, replace number with the following number and display it.

--- src/Display/Display.js
+++ src/Display/Display.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 const Display = ({ displayValue }) => {
+
+  const numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven']
+
+  const convertEnglish = (value) => {
+    const index = numbers.findIndex(number => value.includes(number))
+
+    if (index >= 0 && index < 10) {
+      const wordArray = value.split(' ');
+      const replaceWords = wordArray.map(word => {
+        if (word === numbers[index]) { return numbers[index+1] }
+        else { return word }
+      }).join(' ')
+
+      return replaceWords
+    } else if (index === 10) {
+      return value
+    } else {
+      return value
+    }
+  }
+
   return (
     <div className='display'>
-      {displayValue}
+      {convertEnglish(displayValue)}
     </div>
   )
 }
